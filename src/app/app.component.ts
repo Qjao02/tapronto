f03cad6fbb2cb676e5096e856416c8764939a759
Danilo Amaral <danillodna@gmail.com>
2018-06-11 13:08:58 -0300

    aue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,35 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  sel: number = 1;
-  current: string = '';
+  sel = 0;
+  current = '';
   options = [
-    { desc: ". . .", solution: "" },
-    { desc: "Alocar vetor dinamicamente.", solution: "int* vetor = (int*) calloc(10, sizeof(int));" },
-    { desc: "Alocar matriz dinamicamente.", solution:
-    "int i;\n"+
-    "int** matriz = (int**) calloc(10, sizeof(int*));\n"+
-    "for(i = 0; i < 10; ++i)\n"+
-    "\tmatriz[i] = (int*) calloc(10, sizeof(int));\n"
+    {
+      desc: '. . .',
+      solution: ''
     },
-    { desc: "Inverter uma string.", solution: "EU ME SINTO UM FURACÃO\nNO MEIO DA MULTIDÃO\nDEVORANDO UM MACARRÃO!" },
-    { desc: "Toda string caixa baixa.", solution: "hehehehehihihihi" + "hahahahahahuhuhu" }
+    {
+      desc: 'Como alocar vetor dinamicamente.',
+      solution: 'int* vetor = (int*) calloc(10, sizeof(int));'
+    },
+    {
+      desc: 'Como alocar matriz dinamicamente.',
+      solution:
+        'int i;\n' +
+        'int** matriz = (int**) calloc(10, sizeof(int*));\n' +
+        'for(i = 0; i < 10; ++i)\n' +
+        '\tmatriz[i] = (int*) calloc(10, sizeof(int));\n'
+    },
+    {
+      desc: 'Como inverter uma string.',
+      solution: 'EU ME SINTO UM FURACÃO\nNO MEIO DA MULTIDÃO\nDEVORANDO UM MACARRÃO!'
+    },
+    {
+      desc: 'Como Toda string caixa baixa.',
+      solution: 'hehehehehihihihi' + 'hahahahahahuhuhu'
+    }
   ];
 
-setSelector1() {
-  this.sel = 1;
-}
-setSelector2() {
-  this.sel = 2;
-}
-setSelector3() {
-  this.sel = 3;
-}
-setSelector4() {
-  this.sel = 4;
-}
+  setSelector1() {
+    console.log('OLÁ MARILENE');
+    this.sel = 1;
+  }
+  setSelector2() {
+    this.sel = 2;
+  }
 
-onSelect(current) {
-  this.current = current.solution;
-}
+  onSelect(current) {
+    this.current = current.solution;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SelectorComponent } from './selector/selector.component';
-import { Etp1Component } from './etp1/etp1.component';
-import { Etp2Component } from './etp2/etp2.component';
+// import { SelectorComponent } from './selector/selector.component';
+// import { Etp1Component } from './etp1/etp1.component';
+// import { Etp2Component } from './etp2/etp2.component';
 // import { RepositoryComponent } from './repository/repository.component';
 // import { HeheheComponent } from './hehehe/hehehe.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SelectorComponent,
-    Etp1Component,
-    Etp2Component
+    AppComponent
+    // SelectorComponent,
+    // Etp1Component,
+    // Etp2Component,
     // RepositoryComponent,
     // HeheheComponent
   ],
